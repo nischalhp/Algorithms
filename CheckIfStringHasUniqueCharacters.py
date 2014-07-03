1df6e0d1a327d1880508eef4665d959a6dca2542
Raghotham S <raghotham.s@gmail.com>
2014-07-03 23:10:05 +0530

    converting input to all upper case
    convert all chars to Upper case, because ord() of 'a' & 'A' are different

--- CheckIfStringHasUniqueCharacters.py
+++ CheckIfStringHasUniqueCharacters.py
@@ -3,7 +3,10 @@
 class CheckIfStringHasUniqueCharacters:
 
     def find_if_string_has_unique_chars(self,string):
-
+        
+        # convert all chars to Upper case, because ord of 'a' & 'A' are different
+        string = string.upper()
+        
         character_list = list(string)
         boolean_hash= {}
         for i in xrange(len(character_list)):
